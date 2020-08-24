181e52df381271d612b7829712178534cc3b7d01
Victoria <victoria.sofianidou@gmail.com>
2020-08-24 10:54:26 +0200

    was having errors, took a while to realise that message should appear automatically

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    var Cake = prompt( "Would you like some cake?");
+    if (Cake === "yes"){
+        alert ("Congratulations");
+    }
+    else {
+        alert ("More cake for me then");
+    }
+
+})();
