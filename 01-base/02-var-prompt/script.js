69bb36635a444df8095aa85b52cbe73cac5d5c21
Victoria <victoria.sofianidou@gmail.com>
2020-08-24 10:19:17 +0200

    took a bit more time

---
+++ 01-base/02-var-prompt/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /01-base/02-var-prompt/script.js - 1.2: var & prompt
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+//Ask the name of the user,
+// and display in the dialog box the following welcome message :
+// Hello, [name] !
+
+(function() {
+
+    var name = prompt ("Please enter your name");
+    alert("Hello " + name +"!");
+
+    // your code here
+
+})();
