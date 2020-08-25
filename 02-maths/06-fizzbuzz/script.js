093084498a6a64118ca1794aad58d8bf76575b61
Victoria <victoria.sofianidou@gmail.com>
2020-08-25 16:52:22 +0200

    finally something makes sense to megit add .

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
 
     // your code here
+    for (let i=1; i<=100; i++) {
+
+        if (i%3 === 0 && i%5 === 0) {
+            console.log("fizzbuzz");
+        }
+        else if (i%3 === 0){
+            console.log("fizz");
+        }
+        else if (i%5 === 0){
+            console.log("buzz");
+        }
+        else {
+            console.log(i);
+        }
+
+    }
 
 })();
