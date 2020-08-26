45e8b9d5657f39014e11d689291af26959ab012b
Victoria <victoria.sofianidou@gmail.com>
2020-08-26 14:54:08 +0200

    had trouble with if statement

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
+
+
+(function () {
     // your code here
+    document.getElementById("run").addEventListener("click", function(){
+
+        let day = document.getElementById("dob-day").value;
+        let month = document.getElementById("dob-month").value;
+        let year = document.getElementById("dob-year").value;
+
+        let today = new Date();
+        let age = today.getFullYear() - year;
+
+        if (today.getMonth() < month || (today.getMonth() === month && today.getDate() < day)) {
+            age--;
+        }
+
+        alert(age)
+
+    })
 
 })();
