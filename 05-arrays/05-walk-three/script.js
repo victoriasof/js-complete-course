2b849b62e62b5eb7af19a25baece39c4f9146fae
Victoria <victoria.sofianidou@gmail.com>
2020-08-27 11:26:20 +0200

    5.5 solved using forEach and key.firstname + key.lastname

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -11,34 +11,42 @@
 
 (function() {
 
-    var people = [
-        {
-            firstname: "Garald",
-            lastname: "Freschi",
-            email: "gfreschi0@discuz.net",
-        },
-        {
-            firstname: "Bendicty",
-            lastname: "Foster-Smith",
-            email: "bfostersmith1@mysql.com",
-        },
-        {
-            firstname: "Michelle",
-            lastname: "Draude",
-            email: "mdraude2@csmonitor.com",
-        },
-        {
-            firstname: "Cathee",
-            lastname: "Meineck",
-            email: "cmeineck3@t-online.de",
-        },
-        {
-            firstname: "Marianna",
-            lastname: "Coal",
-            email: "mcoal4@creativecommons.org",
-        },
-    ];
+    document.getElementById("run").addEventListener("click", function(){
 
-    // your code here
+        var people = [
+            {
+                firstname: "Garald",
+                lastname: "Freschi",
+                email: "gfreschi0@discuz.net",
+            },
+            {
+                firstname: "Bendicty",
+                lastname: "Foster-Smith",
+                email: "bfostersmith1@mysql.com",
+            },
+            {
+                firstname: "Michelle",
+                lastname: "Draude",
+                email: "mdraude2@csmonitor.com",
+            },
+            {
+                firstname: "Cathee",
+                lastname: "Meineck",
+                email: "cmeineck3@t-online.de",
+            },
+            {
+                firstname: "Marianna",
+                lastname: "Coal",
+                email: "mcoal4@creativecommons.org",
+            },
+        ];
+
+        // your code here
+        people.forEach(function(person){
+            console.log(person.firstname + " " +person.lastname);
+
+        })
+
+    })
 
 })();
