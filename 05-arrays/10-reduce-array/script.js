a00166e056fdec10f593a7ee435f2485ddbeb55f
Victoria <victoria.sofianidou@gmail.com>
2020-08-28 09:50:21 +0200

    5.10 learned how to add items of an array into another array

--- 05-arrays/10-reduce-array/script.js
+++ 05-arrays/10-reduce-array/script.js
@@ -11,84 +11,96 @@
 
 (function() {
 
-    var people = [
-        {
-            firstname: "Grenville",
-            lastname: "Stive",
-            age: 64,
-        },
-        {
-            firstname: "Luis",
-            lastname: "Jedrzejewsky",
-            age: 73,
-        },
-        {
-            firstname: "Teodor",
-            lastname: "Warrington",
-            age: 56,
-        },
-        {
-            firstname: "Danya",
-            lastname: "Bigby",
-            age: 17,
-        },
-        {
-            firstname: "Jemmy",
-            lastname: "Goodbur",
-            age: 87,
-        },
-        {
-            firstname: "Vilhelmina",
-            lastname: "Fost",
-            age: 43,
-        },
-        {
-            firstname: "Igor",
-            lastname: "Putson",
-            age: 45,
-        },
-        {
-            firstname: "Klement",
-            lastname: "Braybrook",
-            age: 88,
-        },
-        {
-            firstname: "Trefor",
-            lastname: "Le Jean",
-            age: 69,
-        },
-        {
-            firstname: "Tarrah",
-            lastname: "de Clerc",
-            age: 77,
-        },
-        {
-            firstname: "Brad",
-            lastname: "Hookes",
-            age: 82,
-        },
-        {
-            firstname: "Kippar",
-            lastname: "Fancet",
-            age: 25,
-        },
-        {
-            firstname: "Jecho",
-            lastname: "Hawket",
-            age: 87,
-        },
-        {
-            firstname: "Lief",
-            lastname: "Blazy",
-            age: 80,
-        },
-        {
-            firstname: "Jo-ann",
-            lastname: "Sacase",
-            age: 81,
-        },
-    ];
+    document.getElementById("run").addEventListener("click",function(){
 
-    // your code here
+        let people = [
+            {
+                firstname: "Grenville",
+                lastname: "Stive",
+                age: 64,
+            },
+            {
+                firstname: "Luis",
+                lastname: "Jedrzejewsky",
+                age: 73,
+            },
+            {
+                firstname: "Teodor",
+                lastname: "Warrington",
+                age: 56,
+            },
+            {
+                firstname: "Danya",
+                lastname: "Bigby",
+                age: 17,
+            },
+            {
+                firstname: "Jemmy",
+                lastname: "Goodbur",
+                age: 87,
+            },
+            {
+                firstname: "Vilhelmina",
+                lastname: "Fost",
+                age: 43,
+            },
+            {
+                firstname: "Igor",
+                lastname: "Putson",
+                age: 45,
+            },
+            {
+                firstname: "Klement",
+                lastname: "Braybrook",
+                age: 88,
+            },
+            {
+                firstname: "Trefor",
+                lastname: "Le Jean",
+                age: 69,
+            },
+            {
+                firstname: "Tarrah",
+                lastname: "de Clerc",
+                age: 77,
+            },
+            {
+                firstname: "Brad",
+                lastname: "Hookes",
+                age: 82,
+            },
+            {
+                firstname: "Kippar",
+                lastname: "Fancet",
+                age: 25,
+            },
+            {
+                firstname: "Jecho",
+                lastname: "Hawket",
+                age: 87,
+            },
+            {
+                firstname: "Lief",
+                lastname: "Blazy",
+                age: 80,
+            },
+            {
+                firstname: "Jo-ann",
+                lastname: "Sacase",
+                age: 81,
+            },
+        ];
+
+        // your code here
+
+        let ages = [];
+        people.forEach(function (age) {
+            ages.push(age.age);
+        });
+
+        let sum = ages.reduce((a, b) => a + b, 0);
+        console.log(sum);
+
+    })
 
 })();
