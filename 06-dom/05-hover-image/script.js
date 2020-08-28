7427c539f20820f58837672b471fca867c3e6ff5
Victoria <victoria.sofianidou@gmail.com>
2020-08-28 16:00:05 +0200

    6.5 found a simple solution, but turned out to be more complicated

--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -14,9 +14,23 @@
     // When hovering over the image, change its source attribute
     // to the value specified in the "data-hover" attribute
 
+    let image = document.getElementsByTagName("img");
+    let newsource = image[0].dataset.hover;
+    let source = image[0].src;
+
     document.querySelector("img").addEventListener("mouseover", function(){
 
-        this.src = "../../_shared/img/kiss-wink-heart.svg"
+       image[0].setAttribute("src", newsource);
+
+       //ANOTHER WAY: this.src = "../../_shared/img/kiss-wink-heart.svg"
+
+    })
+
+    document.querySelector("img").addEventListener("mouseout", function(){
+
+        image[0].setAttribute("src", source);
+
+        //ANOTHER WAY: this.src = "../../_shared/img/kiss.svg"
 
     })
 
