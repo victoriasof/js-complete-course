5c690dfb1a28f6e2a58f0e877ae22ef45e575b5e
Victoria <victoria.sofianidou@gmail.com>
2020-08-30 21:46:51 +0200

    did exercise 3 again

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -12,10 +12,9 @@
 (function() {
 
     // your code here
-    document.getElementById("run").addEventListener("click", function(){
+    document.getElementById("run").addEventListener("click",function(){
 
         document.body.style.backgroundColor = document.getElementById("color").value;
-
     })
 
-})()
+})();
