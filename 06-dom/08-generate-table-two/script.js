e5536da2fb5c1197429a2e7062a921147e961a74
Victoria <victoria.sofianidou@gmail.com>
2020-08-31 16:48:01 +0200

    Sicco explained how to access cels in tble

--- 06-dom/08-generate-table-two/script.js
+++ 06-dom/08-generate-table-two/script.js
@@ -10,7 +10,52 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // your code here
+    function createTable(){
+
+        let target = document.getElementById("target");
+        let table = document.createElement("table");
+
+        for (let i=0; i<10; i++){
+            let tr = document.createElement("tr");
+
+            for (let j=0; j<10; j++){
+                let td = document.createElement("td");
+
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        target.appendChild(table);
+    }
+    createTable();
+
+    //SICCO EXPLAINED HOW TO ACCESS CELLS IN TABLE AND INSERT CON
+
+    let maketable = document.getElementsByTagName("table");
+    let rows = maketable[0].childNodes;
+
+    //console.log(maketable);
+    //console.log(rows);
+
+    //let firstrow = rows[0];
+    //console.log(firstrow);
+
+    //let cells = firstrow.childNodes;
+    //let firstcell = cells[0];
+    //console.log(cells);
+    //console.log(firstcell);
+
+    //firstcell.innerHTML = "djfhsdjk";
+    //cells[1].innerHTML = "jfjkdsd";
+
+    //rows[9].childNodes[9].innerHTML ="sdjfsj";
+
+    for (let x=1; x<11; x++){
+        for (let y=1; y<11; y++){
+            // += (x*y) + " ";
+            rows[x-1].childNodes[y-1].innerHTML = x*y;
+        }
+    }
 
 })();
