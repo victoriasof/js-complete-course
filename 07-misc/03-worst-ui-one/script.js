3f61d2456d7dd5127ac482f865d7bea02067d4a7
Victoria <victoria.sofianidou@gmail.com>
2020-09-02 14:16:08 +0200

    7.3 can't believe that i solved it myself

--- 07-misc/03-worst-ui-one/script.js
+++ 07-misc/03-worst-ui-one/script.js
@@ -12,5 +12,12 @@
 (function() {
 
     // your code here
+   let slider = document.getElementById("slider");
+   let output = document.getElementById("target");
+
+   output.innerHTML = slider.value;
+   slider.oninput = function (){
+       output.innerHTML = this.value;
+   }
 
 })();
