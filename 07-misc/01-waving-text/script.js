7bea2edb6112dd2448b79de8ec15555a7055511a
Victoria <victoria.sofianidou@gmail.com>
2020-09-02 09:51:51 +0200

    7.1 took too long to solve

---
+++ 07-misc/01-waving-text/script.js
@@ -0,0 +1,35 @@
+/* becode/javascript
+ *
+ * /07-misc/01-waving-text/script.js - 7.1: effet vague
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    let words = document.getElementById("target").innerHTML
+        = "a smooth sea never made a skilled sailor";
+
+    let characters = words.split(" ");
+
+    let wave = " ";
+    let size = [4, 5, 4, 3, 2, 1, 2, 3];
+
+    for (let i = 0; i < characters.length; i++) {
+
+         wave += "<span>" + characters[i] + "</span>";
+
+    }
+
+    document.getElementById("target").innerHTML = wave;
+    let letters = document.getElementsByTagName("span");
+    for (let x=0; x<letters.length; x++){
+        let y = x+1;
+        letters[y].style.fontSize = size[x % size.length] + "0px";
+    }
+
+})();
