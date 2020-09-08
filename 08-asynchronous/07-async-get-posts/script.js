acdcf2940a80e07c2210cc38e32264da50ebcc64
Victoria <victoria.sofianidou@gmail.com>
2020-09-08 16:46:28 +0200

    8.7 solved with async/await syntax

--- 08-asynchronous/07-async-get-posts/script.js
+++ 08-asynchronous/07-async-get-posts/script.js
@@ -9,6 +9,21 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*
+The function window.lib.getPosts() is available in the script.
+It returns a Promise that will be resolved, after a short delay, with a list of articles.
+When you click on the button, call the getPosts function
+and display the items obtained in the console
+(the getPosts function will always be resolved without error).
+Note: for this exercise, use the async/await syntax.
+ */
+
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click",async()=>{
+
+    await window.lib.getPosts().then(console.log);
+
+    })
+
 })();
