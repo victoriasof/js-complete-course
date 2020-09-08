98c335b46c7ae4116ded377e3399a4b26fe2a638
Victoria <victoria.sofianidou@gmail.com>
2020-09-08 09:20:05 +0200

    8.3 similar to 8.1

--- 08-asynchronous/03-handling-errors/script.js
+++ 08-asynchronous/03-handling-errors/script.js
@@ -11,4 +11,17 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        window.lib.getPersons(function (error, array){
+
+            if(error){
+                console.error("error!");
+            }
+            else {
+                console.log(array);
+            }
+        });
+    });
+
 })();
