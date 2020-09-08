d33d1eea78114d70eb5122ffe73f31db650c27b2
Victoria <victoria.sofianidou@gmail.com>
2020-09-08 10:46:45 +0200

    8.4 had trouble, Sicco helped

--- 08-asynchronous/04-promise-get-posts/script.js
+++ 08-asynchronous/04-promise-get-posts/script.js
@@ -9,6 +9,30 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*The function window.lib.getPosts() is available in the script.
+It returns a Promise that will be resolved, after a short delay, with a list of articles.
+When you click on the button, call the getPosts function and display the items obtained in the console
+(the getPosts function will always be resolved without error).
+*/
+
+
+// your code here
+
 (() => {
-    // your code here
+    let run = document.getElementById('run');
+
+    run.addEventListener('click', function () {
+
+        function handleSuccess(resolve) {
+            console.log(resolve);
+        }
+
+        function handleFailure(reject) {
+            console.error(reject);
+        }
+
+        window.lib.getPosts().then(handleSuccess, handleFailure);
+    })
+
 })();
+
