6f1e9c2b93d0d7db46275d0e970b4194bdd19b86
Victoria <victoria.sofianidou@gmail.com>
2020-09-09 15:36:28 +0200

    9.2 solved based on Kayalins example, realised that innerText should be linked with alterEgo and abilities in api.json file

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -9,6 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+//When you click on the button, use the fetch function,
+// retrieve the list of X-Men and display it in the tag whose id is "target".
+// Use the template tag to reproduce an html structure for each hero.
+
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(data => showAllData(data))
+
+            function showAllData(heroes){
+
+                heroes.forEach(hero =>{
+
+                    let templateNode = document.getElementById("tpl-hero").content.cloneNode(true);
+
+                    templateNode.querySelector(".name").innerText = hero.name;
+                    templateNode.querySelector(".alter-ego").innerText = hero.alterEgo;
+                    templateNode.querySelector(".powers").innerText= hero.abilities;
+
+                    document.getElementById("target").appendChild(templateNode);
+
+                })
+
+            }
+    })
 })();
