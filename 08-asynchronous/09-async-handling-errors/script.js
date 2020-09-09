4692ae1eb9db610762f0a18f1d70a37a3758a8eb
Victoria <victoria.sofianidou@gmail.com>
2020-09-09 10:02:37 +0200

    8.9 mixed promise and async/await method

--- 08-asynchronous/09-async-handling-errors/script.js
+++ 08-asynchronous/09-async-handling-errors/script.js
@@ -14,7 +14,7 @@
 
     document.getElementById("run").addEventListener("click", async()=>{
 
-        await window.lib.getPersons().then(console.log).catch(console.log);
+        await window.lib.getPersons().then(console.log).catch(console.error);
 
     })
 
