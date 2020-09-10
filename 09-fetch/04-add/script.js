ff2e434071ee32f7109be0799a2238eb8a61dbb0
Victoria <victoria.sofianidou@gmail.com>
2020-09-10 10:54:54 +0200

    9.4 stuck... have make games

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -9,6 +9,35 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*
+When you click on the button, retrieve the values of the form and create the new hero in the API.
+Each field must be filled in (it is up to you to manage).
+Display the result of all your heroes in the console.
+Caution: powers must be stored in the same format as the previous heroes!
+ */
+
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then(data => addData(data))
+            .catch(err => console.error(err));
+
+        function addData(){
+
+            let name = document.getElementById("hero-name").value;
+            let alterEgo = document.getElementById("hero-alter-ego").value;
+            let powers = document.getElementById("hero-powers").value;
+
+            console.log(name,alterEgo,powers);
+
+            let newHero = document.createElement("form");
+            document.body.appendChild(newHero);
+        }
+
+    })
+
 })();
